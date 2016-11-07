12f90ddbcb556a88a64f449f74afaabc11c4176e
prems021 <premji.netcom@gmail.com>
2016-11-07 13:16:57 +0530

    Update app.module.ts

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,6 @@
-
-
+import './rxjs-extensions';
 import { NgModule }       from '@angular/core';
+import {ReactiveFormsModule} from "@angular/forms";
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { HttpModule }    from '@angular/http';
@@ -9,12 +9,17 @@ import { DashboardComponent }   from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent }      from './signup/signup.component';
 import { TransactionComponent } from './dashboard/transaction/transaction.component';
+import { AccountheadComponent } from './dashboard/transaction/accounthead.component';
 import { VazhipadService } from './dashboard/transaction/vazhipad.service';
 import { routing } from './routing';
 import { AuthGuard } from './authguard';
+import { DataService } from './dashboard/transaction/data.service'
+import { BookingComponent } from './dashboard/transaction/booking.component';
+
 @NgModule({
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     FormsModule,
     HttpModule,
     routing
@@ -22,15 +27,16 @@ import { AuthGuard } from './authguard';
   declarations: [
     AppComponent,
     DashboardComponent,
-    SignupComponent,TransactionComponent,LoginComponent
+    SignupComponent,TransactionComponent,LoginComponent,AccountheadComponent,BookingComponent
   ],
   providers: [
-    AuthGuard,VazhipadService
+    AuthGuard,VazhipadService , DataService
   ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule {
 }
+
 
 
 
