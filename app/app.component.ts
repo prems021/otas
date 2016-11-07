bd094cdff0f825aabe5f1bfbbf14b49b888a94a9
prems021 <premji.netcom@gmail.com>
2016-11-07 13:16:20 +0530

    Update app.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,10 @@
 
-
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   template: `
-    <h1>OTAS (Online Temple Administration System)</h1>
+    <h1>     Online Temple Administration System    </h1>
     <nav>
       <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
       <a routerLink="/login" routerLinkActive="active">Login</a>
