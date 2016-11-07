cc84bef5ca45afef3ddf2d3b2af6505e6cc196bb
prems021 <premji.netcom@gmail.com>
2016-11-07 13:09:36 +0530

    Create zero.ts

---
+++ app/dashboard/transaction/zero.ts
@@ -0,0 +1,13 @@
+export class Zero {
+                    constructor(
+    public quantity: number,
+    public pooja_name: string[],
+    public devotee_name: string,
+    public devotee_star: string,
+    public rate: number,
+    public amount: number,
+    public total: number
+    
+    
+  ) {  }
+}
