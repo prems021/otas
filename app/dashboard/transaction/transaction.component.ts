b450d7924d936b17ac81254ad755da8c57831f5a
prems021 <premji.netcom@gmail.com>
2016-11-07 13:07:36 +0530

    Update transaction.component.ts

--- app/dashboard/transaction/transaction.component.ts
+++ app/dashboard/transaction/transaction.component.ts
@@ -1,12 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+
+
+import { Component,Inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Hero }    from './hero';
+import { Zero }    from './zero';
 import { contentHeaders } from './headers';
 import { Http } from '@angular/http';
 import { Vazhipad }    from './vazhipad';
 import { VazhipadService }    from './vazhipad.service';
 import 'rxjs/add/operator/toPromise';
 import { Observable }        from 'rxjs/Observable';
+import { DataService  } from './data.service';
+import { FormGroup, FormControl, Validators, FormBuilder } 
+    from '@angular/forms';
 
 @Component({
   moduleId: module.id,
@@ -16,38 +21,123 @@ import { Observable }        from 'rxjs/Observable';
 })
 export class TransactionComponent implements OnInit {
 
-   vazhipads: Vazhipad[];
+    vazhipads: Vazhipad[];
+    myForm: FormGroup;
+    active = true;
+    public events: any[] = [];
+   todo = new Zero(1,[],'','',10,10,0);
+     
+  
+    
+  constructor( private fb: FormBuilder, private router: Router, private http: Http, private vazhipadService: VazhipadService , private dataService: DataService
+    ) { 
+     
+    }
 
-  constructor(
-    private router: Router, private http: Http, private vazhipadService: VazhipadService
-    ) { }
+   submitted = false;
+    onSubmit() { 
+         this.submitted = true;
+         this.postdata();
+         console.log('hi');
+     }
+  
   getVazhipad(): void 
   {
     this.vazhipadService.getVazhipad().then(vazhipads => this.vazhipads = vazhipads);
   }
-    ngOnInit(): void {
-    this.getVazhipad()
-      
+
+
+
+
+
+
+    ngOnInit()  {
+     this.getVazhipad()
+     
+   this.myForm = this.fb.group({
+            quantity: ['1'],
+            pooja_name: ['Neyyappam'],
+            devotee_name: ['premji', [<any>Validators.required, <any>Validators.minLength(3)]],
+            devotee_star: ['Aswathy'],
+            rate: ['10'],
+            amount: ['10'],
+            total: ['0']
+
+             
+        });
+
+        
+     this.getVazhipad()
+    this.formchange();
+    
   }
+    
+ formchange()
+{
+
+ 
+  const myFormStatusChanges = this.myForm.statusChanges;
+        const myFormValueChanges = this.myForm.valueChanges;
+        
+       
+    
+  
+}
+
+save(model: Zero, isValid: boolean) {
+        this.submitted = true;
+        console.log(model, isValid);
+    }
+
+
+    
+  
 
   stars = ['Aswathy', 'Bharani','Karthika', 'Makayiarm'];
   
    
   
-  model = new Hero( 1,[],'','',10,10,10);
-  submitted = false;
-  onSubmit() { this.submitted = true; }
-  
-  active = true;
+  //model = new Hero(1,[],'','',10,10);
+
+ // todo = new Zero ({quantity:1,pooja_name:'',devotte_name:'',devotee_star:'',rate:10,amount:10  });
  
-
-
+  heroes: Zero[] = [];
   
 
+
+getamt()
+{
+    
+    this.myForm.patchValue({amount: this.todo.quantity * this.todo.rate  })
+    
+}
+   
+
+
+  addform()
+  { 
+     
+ 
+   this.todo = this.myForm.value;
+   this.myForm.patchValue({amount: this.todo.quantity * this.todo.rate  })
+   this.todo = this.myForm.value;
+   this.myForm.patchValue({total: this.todo.total +++ this.todo.amount})
+   this.todo = this.myForm.value;
+  
+   this.heroes.push(this.todo);
+  
+console.log('hero=',this.heroes);
+
+  
+   
+
+  }
+
+ 
       
-  postdata(){   let body = JSON.stringify(this.model);       
-       
-                    this.http.post('http://localhost:3009/api/newpooja', body, { headers: contentHeaders } )
+  postdata(){   let body = JSON.stringify(this.todo);       
+                    console.log('body=', body);
+                    this.http.post('http://localhost:3009/api/savepooja', body, { headers: contentHeaders } )
                     .subscribe (response => {
            
                     this.router.navigate(['/dashboard']);
